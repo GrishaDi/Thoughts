4006c055be5adf7f2d05678d97c117e5de008a97
Grisha <grishadil.spb@gmail.com>
2021-11-16 07:20:33 +0300

    App-Specific Shared Secret
    For Revenue Cat

--- Thoughts/Managers/IAPManager.swift
+++ Thoughts/Managers/IAPManager.swift
@@ -11,6 +11,9 @@ import Purchases
 final class IAPManager {
     static let shared = IAPManager()
     
+    //RevenueCat Shared Secret
+    //1f0ccde64b784d289dd93c713bf821da
+    
     private init() {}
     
     func isPremium() -> Bool {
