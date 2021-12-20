9c7f77c70d49d51e4ce09964c460e0c4ac0ff6dd
Grisha <grishadil.spb@gmail.com>
2021-12-20 07:24:16 +0300

    Profile

--- Thoughts/Controllers/Core Tabs/ProfileViewController.swift
+++ Thoughts/Controllers/Core Tabs/ProfileViewController.swift
@@ -7,12 +7,58 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // Profile Photo
+    
+    // Full Name
+    
+    // Email
+    
+    // List of Posts
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
+    
+    let currentEmail: String
+    
+    init(currentEmail: String) {
+        self.currentEmail = currentEmail
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
+        setUpSignOutButton()
+        setUpTable()
+        title = currentEmail
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+    }
+    
+    private func setUpTable() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func setUpSignOutButton() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             title: "Sign Out",
             style: .done,
@@ -20,28 +66,42 @@ class ProfileViewController: UIViewController {
             action: #selector(didTapSignOut))
     }
     
-   @objc private func didTapSignOut () {
-       let sheet = UIAlertController(title: "Sign Out", message: "Are you sure you'd like to sign out?", preferredStyle: .actionSheet)
-       sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-       sheet.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { _ in
-           AuthManager.shared.signOut { [weak self] success in
-               if success {
-                   DispatchQueue.main.async {
-                       UserDefaults.standard.set(nil, forKey: "email")
-                       UserDefaults.standard.set(nil, forKey: "name")
-                       
-                       let signInVC = SignInViewController()
-                       signInVC.navigationItem.largeTitleDisplayMode = .always
-                       
-                       let navVC = UINavigationController(rootViewController: signInVC)
-                       navVC.navigationBar.prefersLargeTitles = true
-                       navVC.modalPresentationStyle = .fullScreen
-                       self?.present(navVC, animated: true, completion: nil)
-                   }
-               }
-           }
-       }))
-       
-       present(sheet, animated: true)
+    // Sign Out
+    @objc private func didTapSignOut () {
+        let sheet = UIAlertController(title: "Sign Out", message: "Are you sure you'd like to sign out?", preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { _ in
+            AuthManager.shared.signOut { [weak self] success in
+                if success {
+                    DispatchQueue.main.async {
+                        UserDefaults.standard.set(nil, forKey: "email")
+                        UserDefaults.standard.set(nil, forKey: "name")
+                        
+                        let signInVC = SignInViewController()
+                        signInVC.navigationItem.largeTitleDisplayMode = .always
+                        
+                        let navVC = UINavigationController(rootViewController: signInVC)
+                        navVC.navigationBar.prefersLargeTitles = true
+                        navVC.modalPresentationStyle = .fullScreen
+                        self?.present(navVC, animated: true, completion: nil)
+                    }
+                }
+            }
+        }))
+        
+        present(sheet, animated: true)
+    }
+    
+    // TableView
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Blog post goes here!"
+        
+        return cell
     }
 }
