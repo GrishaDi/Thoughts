f7a06c728c5c528505879564595e67587087a788
Grisha <grishadil.spb@gmail.com>
2022-01-07 11:50:04 +0300

    The Button for Composing Posts

--- Thoughts/Controllers/Core Tabs/HomeViewController.swift
+++ Thoughts/Controllers/Core Tabs/HomeViewController.swift
@@ -9,13 +9,42 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let composeButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.setImage(UIImage(systemName: "square.and.pencil",
+                                withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium)),
+                                for: .normal)
+        button.layer.cornerRadius = 40
+        button.layer.shadowColor = UIColor.label.cgColor
+        button.layer.shadowOpacity = 0.5
+        button.layer.shadowRadius = 7
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
-        
+        view.addSubview(composeButton)
+        composeButton.addTarget(self, action: #selector(didTapCreate), for: .touchUpInside)
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        composeButton.frame = CGRect(x: view.frame.width - 80 - 16,
+                                     y: view.frame.height - 80 - 16 - view.safeAreaInsets.bottom,
+                                     width: 80,
+                                     height: 80)
+    }
 
+    @objc private func didTapCreate() {
+        let vc = CreateNewPostViewController()
+        vc.title = "Create Posts"
+        let navVC = UINavigationController(rootViewController: vc)
+        present(navVC, animated: true)
+    }
 }
 
